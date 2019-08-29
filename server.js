295557955a9b291a35b31208639dd4ab05678e62
Dana G <32311532+dastryyy@users.noreply.github.com>
2019-08-29 15:47:25 -0400

    Finished

--- server.js
+++ server.js
@@ -6,24 +6,39 @@ var http = require('http'),
 /* Global variables */
 var listingData, server;
 
-var requestHandler = function(request, response) {
-  var parsedUrl = url.parse(request.url);
+var requestHandler = function (request, response) {
+    var parsedUrl = url.parse(request.url);
 
-  /*
-    Your request handler should send listingData in the JSON format as a response if a GET request 
-    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
+    /*
+      Your request handler should send listingData in the JSON format as a response if a GET request 
+      is sent to the '/listings' path. Otherwise, it should send a 404 error. 
+  
+      HINT: Explore the request object and its properties 
+      HINT: Explore the response object and its properties
+      https://code.tutsplus.com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
+      http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
+      
+      HINT: Explore how callback's work 
+      http://www.theprojectspot.com/tutorial-post/nodejs-for-beginners-callbacks/4
+      
+      HINT: Explore the list of MIME Types
+      https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
+     */
+    
 
-    HINT: Explore the request object and its properties 
-    HINT: Explore the response object and its properties
-    https://code.tutsplus.com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
-    http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
-    
-    HINT: Explore how callback's work 
-    http://www.theprojectspot.com/tutorial-post/nodejs-for-beginners-callbacks/4
-    
-    HINT: Explore the list of MIME Types
-    https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
-   */
+    if (parsedUrl.pathname == '/listings' && request.method == 'GET') { 
+        //send listingData in JSON format
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
+        response.write(JSON.stringify(listingData));
+        response.end();
+    }
+    else {
+        //send 404 error
+        response.writeHead(404, { 'Content-Type': 'text/plain' });
+        response.write('Bad gateway error');
+        response.end();
+    }
+
 };
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
@@ -36,16 +51,21 @@ fs.readFile('listings.json', 'utf8', function(err, data) {
 
     HINT: Read up on JSON parsing Node.js
    */
+    
 
     //Check for errors
+    if (err) throw err;
   
 
-   //Save the sate in the listingData variable already defined
-  
+    //Save the sate in the listingData variable already defined
+    listingData = JSON.parse(data);
 
-  //Creates the server
+    //Creates the server
+    server = http.createServer(requestHandler);
   
   //Start the server
+    server.listen(port);
+    console.log('Server started');
 
 
 });
